adcc0d19703d38e77ca6bcdb775c714bd77bd863
Alexandr Trifonov <lordsanchez@yandex.ru>
2023-05-11 16:32:49 +0400

    add default pagination class

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -104,7 +104,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
-
-# test
-# check 2
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -14,8 +14,8 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
